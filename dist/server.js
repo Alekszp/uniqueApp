3079b7ceb59b626ba48f11d7c387324047499df2
Aleksandr Zlt <fasdir@ukr.net>
2019-02-17 22:16:11 +0200

    com 2

--- dist/server.js
+++ dist/server.js
@@ -10,10 +10,7 @@ var app = (0, _express.default)();
 app.use('/dist', _express.default.static('./dist'));
 app.get('/*', function (req, res) {
   res.sendFile(_path.default.join(__dirname, '..', 'dist', 'index.html'));
-}); // app.get('/about', (req, res)=> {
-//     res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
-// });
-
+});
 app.listen(7001, '0.0.0.0', function () {
   console.log('Server is up to running on 7001 port');
 });
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
 
 const Router = BrowserRouter;
 
@@ -19,7 +19,9 @@ class About extends Component {
         super(props);
     }
     render() {
-        return <h2>About</h2>;
+        const id = this.props.match.params.id;
+        const name = this.props.match.params.name;
+        return <h2>id: {id} Name: {name}</h2>;
     }
 }
 class NotFound extends Component {
@@ -30,16 +32,58 @@ class NotFound extends Component {
         return <h2>Not found</h2>;
     }
 }
+class Product extends Component {
+    render() {
+        const prodId = this.props.match.params.id;
+        return (
+            <h2>Product #: {prodId}</h2>
+        )
+    }
+}
+class ProductList extends Component {
+    render() {
+        return (
+            <h2>Products list</h2>
+        )
+    }
+}
+class Products extends Component {
+    render() {
+        return (
+            <div>
+                <Switch>
+                    <Route exact path="/products" component={ProductList} />
+                    <Route path="/products/:id" component={Product} />
+                </Switch>
+            </div>
+        )
+    }
+}
+class Nav extends Component {
+    render() {
+        return (
+            <nav>
+                <NavLink exact to="/" activeClassName="active">Main</NavLink>
+                <NavLink to="/about" activeClassName="active">About</NavLink>
+                <NavLink to="/products" activeClassName="active">Products</NavLink>
+            </nav>
+        )
+    }
+}
 
 const wrapper = document.getElementById("app");
 wrapper ? ReactDOM.render(
     <Router>
-        <Switch>
-            <Route exact path='/' component={Main} />
-            <Route path='/about' component={About} />
-            <Route component={NotFound} />
-        </Switch>
+        <div>
+            <Switch>
+                <Route exact path='/' component={Main} />
+                <Route path='/about/:id/:name' component={About} />
+                <Route path='/products' component={Products} />
+                <Route component={NotFound} />
+            </Switch>
+        </div>
+
     </Router>,
     wrapper) : false;
 
-export {Router, Switch, Route};
+export { Router, Switch, Route };
